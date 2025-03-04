e51d366e59c6246e0b28aa04e5ad4208dc57356b
NKDShinKu <2010182879@qq.com>
2025-03-04 09:48:46 +0800

    feat(CvTemplate): 增加模板详情切换

--- src/views/CvTemplate/CvTemplate.vue
+++ src/views/CvTemplate/CvTemplate.vue
@@ -18,9 +18,14 @@ const mockData = [
   { id: '11', title: '工程师专用', imageUrl: 'src/assets/img/default-resume.png', style: '工程', industry: '建筑', color: '钢蓝', size: '225*315' },
   { id: '12', title: '实习生简历', imageUrl: '/src/assets/img/default-resume.png', style: '实习', industry: '大学生', color: '青色', size: '210*290' }
 ];
+const select1 = ref("");
+const select2 = ref("");
+const select3 = ref("");
+const options = ["风格1", "风格2", "风格3", "风格4"];
 
 const selectedIndex = ref(0); // 当前选中的简历索引
 const showMenu = ref(false); // 控制菜单显示与隐藏的状态
+
 // 切换菜单显示状态
 const toggleMenu = (index: number) => {
   showMenu.value = !showMenu.value;
@@ -31,16 +36,14 @@ const toggleMenu = (index: number) => {
 // 切换上一张
 const prevCv = () => {
   if (selectedIndex.value > 0) selectedIndex.value--;
+  scrollToTop();
 };
 // 切换下一张
 const nextCv = () => {
   if (selectedIndex.value < mockData.length - 1) selectedIndex.value++;
+  scrollToTop();
 };
 
-const select1 = ref("");
-const select2 = ref("");
-const select3 = ref("");
-const options = ["风格1", "风格2", "风格3", "风格4"];
 const scrollToTop = () => {
   window.scrollTo({
     top: 0,
@@ -61,7 +64,7 @@ const scrollToTop = () => {
         <div class="flex ml-2">
           <div>
             <select v-model="select1"
-              class="w-[5.2rem] h-[2.4rem]  mr-[1.6rem] text-[#505050] rounded-lg border-2 border-solid border-[#cdcdcd] text-center pr-[0.5rem] text-[16px] focus:outline-none">
+              class="w-[5.4rem] h-[2.5rem]  mr-[1.6rem] text-[#505050] rounded-lg border-2 border-solid border-[#cdcdcd] text-center pr-[0.5rem] text-[16px] focus:outline-none">
               <option value="">风格</option>
               <option v-for="item in options" :key="item" :value="item">
                 {{ item }}
@@ -70,7 +73,7 @@ const scrollToTop = () => {
           </div>
           <div>
             <select v-model="select2"
-              class="w-[5.2rem] h-[2.4rem]  mr-[1.6rem] text-[#505050] rounded-lg border-2 border-solid border-[#cdcdcd] text-center pr-[0.5rem] text-[16px] focus:outline-none">
+              class="w-[5.4rem] h-[2.5rem]  mr-[1.6rem] text-[#505050] rounded-lg border-2 border-solid border-[#cdcdcd] text-center pr-[0.5rem] text-[16px] focus:outline-none">
               <option value="">行业</option>
               <option v-for="item in options" :key="item" :value="item">
                 {{ item }}
@@ -79,7 +82,7 @@ const scrollToTop = () => {
           </div>
           <div>
             <select v-model="select3"
-              class="w-[5.2rem] h-[2.4rem]  mr-[1.6rem] text-[#505050] rounded-lg border-2 border-solid border-[#cdcdcd] text-center pr-[0.5rem] text-[16px] focus:outline-none">
+              class="w-[5.4rem] h-[2.5rem]  mr-[1.6rem] text-[#505050] rounded-lg border-2 border-solid border-[#cdcdcd] text-center pr-[0.5rem] text-[16px] focus:outline-none">
               <option value="">颜色</option>
               <option v-for="item in options" :key="item" :value="item">
                 {{ item }}
@@ -89,7 +92,7 @@ const scrollToTop = () => {
         </div>
 
         <!-- 搜索区域 -->
-        <div class="flex items-center mr-16 w-[16.2rem] h-[2.4rem] rounded-lg bg-[#3370ff]">
+        <div class="flex items-center mr-16 w-[16.2rem] h-[2.5rem] rounded-lg bg-[#3370ff]">
           <input type="text" placeholder="搜索简历模板"
             class="flex-1 pl-[1.2rem] h-full bg-white border rounded-lg focus:outline-none focus:border-blue-500">
           <img src="@/assets/img/CvTemplate/search.svg" class="h-full inline-block" alt="搜索">
@@ -107,17 +110,22 @@ const scrollToTop = () => {
     <!-- 引用菜单组件，并绑定显示状态 -->
     <div v-if="showMenu" class="absolute top-0 bg-black opacity-25 w-full h-full">
     </div>
+    <button v-if="showMenu"
+      class="fixed z-10  top-1/2 left-24 w-10 h-10  bg-[url(@/assets/img/CvTemplate/navigate_before.webp)] bg-cover transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-0.5"
+      @click="prevCv"></button>
+    <button v-if="showMenu"
+      class="fixed z-10 top-1/2 right-24 w-10 h-10 bg-[url(@/assets/img/CvTemplate/navigate_next.webp)] bg-cover transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-0.5"
+      @click="nextCv"></button>
 
     <Menu class="absolute h-full top-0 left-0 right-0" :cvData="mockData[selectedIndex]" :isVisible="showMenu"
       @update:isVisible="showMenu = $event">
     </Menu>
 
-    <button class="fixed  top-1/2 left-8 w-10 h-10 bg-white" @click="prevCv">上一张</button>
-    <button class="fixed  top-1/2 right-8 w-10 h-10 bg-white" @click="nextCv">下一张</button>
+
     <!-- 返回顶部 -->
     <button @click="scrollToTop"
-      class="fixed bottom-8 right-8 w-10 h-10 bg-white shadow-md rounded-full flex items-center justify-center hover:shadow-lg transition-shadow !rounded-button whitespace-nowrap">
-      <i class="fas fa-arrow-up text-gray-600"></i>
+      class="fixed bottom-8 right-22 w-12 h-12 bg-[url(@/assets/img/CvTemplate/top.webp)] bg-cover  rounded-full flex items-center justify-center hover:shadow-lg transition-shadow !rounded-button whitespace-nowrap">
+
     </button>
   </div>
 </template>
