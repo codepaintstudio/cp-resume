f619c236acb8e7f225be6058c9ea43709a85ebb0
ababab123 <14425780+abaabab123@user.noreply.gitee.com>
2025-03-09 20:14:21 +0800

    fix:修复页面报错

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // 移除动画配置
+    }
+  },
+  plugins: [],
+}
