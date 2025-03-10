eaf77a5847b15486002d419d84bb3daf86505d2a
NKDShinKu <2010182879@qq.com>
2025-03-10 16:31:51 +0800

    style: 修改部分目录名称

--- src/views/edit/components/CvPreview.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-</script>
-<template>
-  <div>预览</div>
-</template>
-<style scoped></style>
--- src/views/user/components/CvAdmn.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-</script>
-<template>
-  <div>CvAdmn</div>
-</template>
-<style scoped lang="scss"></style>
