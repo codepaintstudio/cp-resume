da35d61fb589bcbdcb9d9bf7cc75b06da97242cd
NKDShinKu <2010182879@qq.com>
2025-03-18 15:15:50 +0800

    style: 安装图标库, 修改编辑页面图标

--- src/views/edit/components/CvHeader.vue
+++ src/views/edit/components/CvHeader.vue
@@ -11,14 +11,15 @@
 
       <!--撤销与云端 -->
       <div class="ml-18 flex items-center space-x-6">
-        <button class="text-gray-600 hover:text-blue-500 hover:scale-107 active:scale-100">
-          <img src="@/assets/img/edit/header_undo.svg" class="w-4" alt="">
+        <button class="text-gray-600 hover:text-blue-500 hover:scale-107 active:scale-100 transition-all duration-200">
+          <span class="icon-[la--undo] text-xl"></span>
         </button>
-        <button class="text-gray-600 hover:text-blue-500 hover:scale-107 active:scale-100">
-          <img src="@/assets/img/edit/header_redo.svg" class="w-4" alt="">
+        <button class="text-gray-600 hover:text-blue-500 hover:scale-107 active:scale-100 transition-all duration-200">
+          <span class="icon-[la--redo] text-xl"></span>
         </button>
-        <button class="text-gray-600 hover:text-blue-500 ml-2 hover:scale-105 active:scale-100">
-          <img src="@/assets/img/edit/header_cloud.svg" class="w-6" alt="">
+        <button
+          class="text-gray-600 hover:text-blue-500 ml-2 hover:scale-105 active:scale-100 transition-all duration-200">
+          <span class="icon-[solar--cloud-download-outline] text-xl"></span>
         </button>
       </div>
 
@@ -29,11 +30,11 @@
     <!-- 右侧按钮 -->
     <div class="flex items-center space-x-8 mr-10">
       <button class="h-[35px] flex items-center space-x-3 px-4 py-2 border border-black rounded hover:bg-gray-100">
-        <img src="@/assets/img/edit/header_share.svg" class="h-3/4" alt="">
+        <span class="icon-[teenyicons--share-outline]"></span>
         <span>分享</span>
       </button>
       <button class="h-[35px] flex items-center space-x-3 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
-        <img src="@/assets/img/edit/header_download.svg" class="h-3/4" alt="">
+        <span class="icon-[uil--export]"></span>
         <span>导出</span>
       </button>
     </div>
