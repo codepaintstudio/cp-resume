06b39e400c957dbbf337c4e13d6506cbf669c244
minorcell <minorcell6789@gmail.com>
2025-05-01 16:43:36 +0800

    fixd user module

--- src/components/CvCard.vue
+++ src/components/CvCard.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { defineProps, computed } from 'vue'
-import type { Template } from '@/types/template'
 import cvImg from '/default_cv.png'
 
 interface cvTemplate {
@@ -27,36 +26,43 @@ const props = defineProps({
     type: Object as () => cvTemplate,
     default: () => ({
       resumeTemplateName: '标题',
-      resumeTemplateContent:{
+      resumeTemplateContent: {
         folderPath: 'templateA',
         thumbnail: '',
         style: '风格',
         industry: '行业',
         color: '颜色',
-      }
-
+      },
     }),
   },
 })
 
 const scale = computed(() => parseFloat(props.size))
 const customStyle = computed(() => {
-  const baseWidth = 280  // 对应 w-70，70 * 4 = 280px
+  const baseWidth = 280 // 对应 w-70，70 * 4 = 280px
   const baseHeight = 400 // 对应 h-100，100 * 4 = 400px
   return {
     width: `${baseWidth * scale.value}px`,
     height: `${baseHeight * scale.value}px`,
   }
 })
-
 </script>
 <template>
   <div
     :style="customStyle"
-    :class="['bg-white rounded-lg shadow-[0px_0px_8px_-3px] hover:shadow-[0px_0px_15px_-5px] transition-shadow']">
-    <img :src="cvTemplate.resumeTemplateContent.thumbnail || cvImg"
-      :class="{ 'w-full object-cover object-top rounded-t-lg': true, 'h-3/4': isView, 'h-5/6': !isView }"
-      :alt="`简历模板`" />
+    :class="[
+      'bg-white rounded-lg shadow-[0px_0px_8px_-3px] hover:shadow-[0px_0px_15px_-5px] transition-shadow',
+    ]"
+  >
+    <img
+      :src="cvTemplate.resumeTemplateContent.thumbnail || cvImg"
+      :class="{
+        'w-full object-cover object-top rounded-t-lg': true,
+        'h-3/4': isView,
+        'h-5/6': !isView,
+      }"
+      :alt="`简历模板`"
+    />
     <div class="p-3">
       <div class="flex items-center justify-between">
         <h3 class="text-gray-800 font-medium">{{ cvTemplate.resumeTemplateName }}</h3>
